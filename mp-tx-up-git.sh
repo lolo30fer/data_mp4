f9a83c9b2a0e73649878b28cc9001bbf72c8d0cc
lolo30fer <lolo30fer@gmail.com>
2022-01-31 03:05:09 -0500

    tig

--- mp-tx-up-git.sh
+++ mp-tx-up-git.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 git init
 sleep 5
+git config --global user.email "lolo30fer@gmail.com"
+sleep 5
+git config --global user.name "lolo30fer"
+sleep 5
 git add -A
 sleep 5
 git commit -m "tig"
 sleep 5
 git branch -M main
 sleep 5
-git remote add origin https://github.com/lolo30fer/data_mp4.git
+git remote add origin https://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@github.com/lolo30fer/data_mp4.git
 sleep 5
 git push -u origin main
